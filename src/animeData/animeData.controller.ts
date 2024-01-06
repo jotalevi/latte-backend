46a220a21718b736c7465a6a420bcc13e60a7fbc
jotalevi <etalevi@moveapps.cl>
2024-01-05 22:50:48 -0300

    No commit msg

--- src/animeData/animeData.controller.ts
+++ src/animeData/animeData.controller.ts
@@ -70,7 +70,7 @@ export class AnimeDataController {
   async getAnimeEpisode(
     @Param('id') id: string,
     @Param('ep') ep: string,
-    @Param('dev') dev: boolean,
+    //@Param('dev') dev: boolean,
     @Headers('Authorization') token: string,
   ): Promise<ReturnEpisodeDto | ReturnErrorDto> {
     if (!(await this.userService.checkToken(token.split(' ')[1])))
@@ -84,7 +84,7 @@ export class AnimeDataController {
       anime: id,
       episodes: ep,
     });
-    return Scraper.episode(id, ep, dev);
+    return Scraper.episode(id, ep, false);
   }
 
   @Get('search/')
